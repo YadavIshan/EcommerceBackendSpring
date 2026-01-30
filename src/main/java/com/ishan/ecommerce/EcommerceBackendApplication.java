047fba1ee947849341018fc4f24aa94ab2b074e1
Ishan Yadav <37396882+YadavIshan@users.noreply.github.com>
2026-01-31 00:28:57 +0530

    Enabled eureka client

--- src/main/java/com/ishan/ecommerce/EcommerceBackendApplication.java
+++ src/main/java/com/ishan/ecommerce/EcommerceBackendApplication.java
@@ -4,9 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
